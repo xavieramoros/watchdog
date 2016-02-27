f8c7713816f9c39a81af779be7f5de5243809d18
Xavi Amoros <xaviamoros@gmail.com>
2016-02-27 14:25:04 +0100

    Added mongo connection, urls view and newurl input form

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//MongoDB settings:
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/watchdog');
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +27,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Make our db accessible to our router, wrapping database object into every request //FIXME
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
+
+/* GET urls. */
+router.get('/urls', function(req, res, next) {
+  var db = req.db;  
+  var urlcollection = db.get('urlcollection');
+  urlcollection.find({},{},function(e,docs){
+      if(e) console.log("ERROR:",e);
+      
+      console.log("DOCS:",docs);
+
+      res.render('urls', {
+          "urlList" : docs
+      });
+  });
+});
+
+/* GET New URL page. */
+router.get('/newurl', function(req, res) {
+    res.render('newurl', { title: 'Add New Url' });
+});
+
+/* POST to Add Url Service */
+router.post('/addurl', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var url = req.body.url;
+    var title = req.body.title;
+
+    // Set our collection
+    var collection = db.get('urlcollection');
+
+    // Submit to the DB
+    collection.insert({
+        "url" : url,
+        "title" : title
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("urls");
+        }
+    });
+});
+
+
+/* GET specific url
+router.get('/url/:id', function(req, res, next) {
+  res.render('urls', { title: 'Express' });
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
