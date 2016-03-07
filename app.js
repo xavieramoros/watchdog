1729361fa42415dc20ae72c34d118c9e63b73176
Xavi Amoros <xaviamoros@gmail.com>
2016-03-07 01:19:00 +0100

    Adding server to app.listen for openshift

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080);
+//app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+//app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
